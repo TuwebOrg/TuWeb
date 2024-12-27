a80f073ebc59dc86c86b8cb972fe7bbbbf6039e4
Diego <diegovalrus@gmail.com>
2024-12-26 20:17:46 -0500

    Mobile First Optimized

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const callErnesto = document.getElementById('call_ernesto')
     const callDiego = document.getElementById('call_diego')
     const wppErnesto = document.getElementById('wpp_ernesto')
-    const wppDiego = document.getElementById('wpp_diego')    
+    const wppDiego = document.getElementById('wpp_diego')   
+    const btnIg = document.getElementById('btn_ig') 
+    const btnTg = document.getElementById('btn_tg') 
 
     // Boton de inicio
     btnLogo.addEventListener('click', () => {
@@ -45,11 +47,18 @@ document.addEventListener('DOMContentLoaded', () => {
     wppDiego.addEventListener('click', () => {
         window.location.href = 'https://wa.me/+5356938821?text=Hola!'
     })
+    // Botones de redes sociales
+    btnIg.addEventListener('click', () => {
+        window.open('https://www.instagram.com/tuweb_cu?igsh=cTFnZnB0NDRsbmM0', '_blank')
+    })
+    btnTg.addEventListener('click', () => {
+        window.open('https://t.me/tuweb_cu', '_blank')
+    })
 
     // Transición borrosa
-    const background = document.getElementById('top')
+    const background = document.getElementById('port')
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 90) background.classList.add('blur')
+        if (window.scrollY > 60) background.classList.add('blur')
         else background.classList.remove('blur')
     })
 })
